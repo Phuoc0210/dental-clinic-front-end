958304e9e3709df2f212a2cc99cfb186e5d8afdb
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2022-11-03 22:10:04 +0700

    add 3 first files to homepage

---
+++ dental-clinic/src/pages/homepage/Slider/SliderBanner.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import Slider1 from "../../../assets/homepageBG1.png";
+import Slider2 from "../../../assets/homepageBG2.png";
+import Slider3 from "../../../assets/homepageBG3.png";
+import { BsFillCalendar2CheckFill } from "react-icons/bs";
+
+export default function SliderBanner() {
+  return (
+    <>
+      <div style={{ marginTop: "120px" }}></div>
+
+      <div
+        style={{
+          zIndex: "100",
+          position: "absolute",
+          top: "550px",
+          right: "60px",
+          fontSize: "40px",
+          color: "#b0228c",
+        }}
+      >
+        <BsFillCalendar2CheckFill />
+      </div>
+
+      <div
+        style={{
+          zIndex: "100",
+          position: "absolute",
+          top: "550px",
+          right: "120px",
+          fontSize: "40px",
+        }}
+      >
+        <button
+          type="button"
+          class="btn customOrderBtn"
+          style={{ backgroundColor: "#b0228c", color: "#fff" }}
+        >
+          ĐẶT LỊCH HẸN
+        </button>
+      </div>
+
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-bs-ride="true"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={Slider1} className="d-block w-100" alt="slider 1" />
+          </div>
+          <div className="carousel-item">
+            <img src={Slider2} className="d-block w-100" alt="slider 2" />
+          </div>
+          <div className="carousel-item">
+            <img src={Slider3} className="d-block w-100" alt="slider 3" />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+}
